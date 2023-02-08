070813999a4c1edc71ff97d69712d3b3535fe3a3
Connor White <connorwhitepdx@gmail.com>
2023-02-07 22:54:08 -0800

    animations dialed

--- src/pages/content.js
+++ src/pages/content.js
@@ -1,8 +1,11 @@
 import styles from '@/styles/content.module.css';
 import Image from 'next/image';
+// Image imports
 import bioPic from '../../public/biopic.png';
+import painting from '../../public/painting.png';
+// Font imports
 import { Baskervville } from '@next/font/google';
-import { circIn, easeIn, motion } from 'framer-motion';
+import { easeIn, motion } from 'framer-motion';
 
 const baskervville = Baskervville({ 
     subsets: ["latin"],
@@ -10,11 +13,28 @@ const baskervville = Baskervville({
     styles: ['italic', 'normal'],
 });
 
+const baskerville = Baskervville({ 
+    subsets: ["latin"],
+    weight: "400",
+    style: 'italic',
+});
+
 export default function Content() {
     return (
         <div className={styles.container}>
+            <motion.div className={styles.loader} initial={{ display: null }} animate={{display: 'none'}} transition={{delay: 4}}>
+                <motion.div className={styles.loaderName} style={baskervville.style} animate={{ opacity: [0, 1, 1, 0]}} transition={{ duration: 4, times: [0, 0.25, 0.75, 1]}}>
+                    Birth & Death Doula
+                </motion.div>
+                <motion.div className={styles.loaderImage} animate={{ opacity: [0, 1, 1, 0]}} transition={{ duration: 4, times: [0, 0.25, 0.75, 1]}}>
+                    <Image src={painting} alt="Loading Painting" style={{ width: '100%', height: '100%'}}/>
+                </motion.div>
+                <motion.div className={styles.loaderLocation} style={baskerville.style} animate={{ opacity: [0, 1, 1, 0]}} transition={{ duration: 4, times: [0, 0.25, 0.75, 1]}}>
+                    Los Angeles, California
+                </motion.div>
+            </motion.div>
             <div className={styles.bio}>
-                <motion.div className={styles.copy} style={baskervville.style} initial={{ opacity: 0, x: 50 }} animate={{ opacity: 1, x: 0 }} transition={{ type: 'spring', bounce: 0, ease: easeIn, duration: 1, delay: 0.5 }}>
+                <motion.div className={styles.copy} style={baskervville.style} initial={{ opacity: 0, x: 50 }} animate={{ opacity: 1, x: 0 }} transition={{ type: 'spring', bounce: 0, ease: easeIn, duration: 1, delay: 4.5 }}>
                     Whether you’re preparing for birth and postpartum, facing the loss of your baby, or are approaching end of life, these profound human experiences require tender support and guidance.
                     <br/>
                     <br/>
@@ -32,11 +52,11 @@ export default function Content() {
                     <br/>
                     More to come :&#41;
                 </motion.div>
-                <motion.div className={styles.image} initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ ease: easeIn, delay: 0.5, duration: 1 }}>
+                <motion.div className={styles.image} initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ ease: easeIn, delay: 4.5, duration: 1 }}>
                     <Image src={bioPic} alt="Jess Ford" style={{ width: '100%', height: '100%', border: '1px solid #2B3034'}}/>
                 </motion.div>
             </div>
-            <motion.div className={styles.cta} initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+            <motion.div className={styles.cta} initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 4.5 }}>
                 <a href='https://www.calendly.com/jessfordcare' target="_blank" rel="noreferrer"><motion.button className={styles.button} style={baskervville.style} whileTap={{ scale: 0.97 }} whileHover={{ backgroundColor: '#2B3034', color: 'white' }}>CONNECT</motion.button></a>
             </motion.div>
         </div>
--- src/pages/header.js
+++ src/pages/header.js
@@ -12,8 +12,8 @@ export default function Header() {
                 <Image src={logoImg} alt="Jess Ford" height={100} />
             </motion.div>
             <div className={styles.icons}>
-                <motion.a className={styles.icon} href="https://www.instagram.com/jessford.care/" target="_blank" rel="noreferrer" initial={{ scale: 0, opacity: 0 }} animate={{ scale: 1, opacity: 1 }} transition={{ type: 'spring', delay: 0.2 }} whileHover={{ scale: 1.1 }}><Image src={insta} alt="Instagram Link"/></motion.a>
-                <motion.a className={styles.icon} href="mailto:jessfordcare@gmail.com" initial={{ scale: .5, opacity: 0 }} animate={{ scale: 1, opacity: 1 }} transition={{ type: 'spring', delay: 0.3 }} whileHover={{ scale: 1.1 }}><Image src={email} alt="Email Link"/></motion.a>
+                <motion.a className={styles.icon} href="https://www.instagram.com/jessford.care/" target="_blank" rel="noreferrer" initial={{ scale: 0, opacity: 0 }} animate={{ scale: 1, opacity: 1 }} transition={{ type: 'spring', delay: 4.2 }} ><Image src={insta} alt="Instagram Link"/></motion.a>
+                <motion.a className={styles.icon} href="mailto:jessfordcare@gmail.com" initial={{ scale: .5, opacity: 0 }} animate={{ scale: 1, opacity: 1, transition: {type: "spring", delay: 4.3}}} ><Image src={email} alt="Email Link"/></motion.a>
             </div>
         </div>
     )
